1b0520ebd35548abdaaae820a973a78ab2a0d306
Fabio Demarqui <fndemarqui@est.ufmg.br>
2024-03-20 23:00:02 +0000

    version 0.0.7

--- inst/doc/LRT.R
+++ inst/doc/LRT.R
@@ -24,12 +24,18 @@ km <- survfit(Surv(time, status) ~ arm, data = ipass)
 ggsurv(km) 
 
 ## -----------------------------------------------------------------------------
+aft <- aftreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
+ah <- ahreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
 ph <- phreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
 po <- poreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
 yp <- ypreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
-
+eh <- ehreg(Surv(time, status)~arm, data=ipass, dist = "weibull")
 
 anova(ph, yp)
 anova(po, yp)
+
+anova(aft, eh)
+anova(ah, eh)
+anova(ph, eh)
 
 
